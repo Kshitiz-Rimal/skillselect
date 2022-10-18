41f0c01a19470fbfd0315749a99c45cc4705e5af
Kshitiz Rimal <kshitizrimal123@gmail.com>
2022-10-18 14:51:54 +0545

    made properly

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,100 +1,64 @@
 class MyFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-    <section class="above-footer pt-3">
-      <div class="content">
-        <div class="Services">
-          <label>Our Services</label><br />
-          <a href="#" class="bi-chevron-right">CDR Report Writing</a><br /><br />
-          <a href="#" class="bi-chevron-right">Career Episode Writing</a><br /><br />
-          <a href="#" class="bi-chevron-right">Summary Statement</a><br /><br />
-          <a href="#" class="bi-chevron-right">ACS RPL Report Writing</a><br /><br />
-          <a href="#" class="bi-chevron-right">KAO2 Knowledge assessment</a><br /><br />
-          <a href="#" class="bi-chevron-right">CDR Report Review Services</a><br /><br />
-        </div>
+      <section id="above_footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-3">
+              <div class="Services">
+                <label>Our Services</label><br />
+                <a href="#" class="bi-chevron-right">CDR Report Writing</a><br /><br />
+                <a href="#" class="bi-chevron-right">Career Episode Writing</a><br /><br />
+                <a href="#" class="bi-chevron-right">Summary Statement</a><br /><br />
+                <a href="#" class="bi-chevron-right">ACS RPL Report Writing</a><br /><br />
+                <a href="#" class="bi-chevron-right">KAO2 Knowledge assessment</a><br /><br />
+                <a href="#" class="bi-chevron-right">CDR Report Review Services</a><br /><br />
+              </div>
+            </div>
+          
 
-        <div class="Blogs">
-          <label>Our Blogs</label><br />
-          <a href="#" class="bi-chevron-right"
-            >Lorem ipsum dolor, sit amet consectetur adipisicing elit.</a
-          ><br /><br />
-          <a href="#" class="bi-chevron-right"
-            >Lorem ipsum dolor sit amet consectetur adipisicing elit.</a
-          ><br /><br />
-          <a href="#" class="bi-chevron-right"
-            >Lorem ipsum dolor sit, amet consectetur adipisicing elit.</a
-          ><br /><br />
-          <a href="#" class="bi-chevron-right"
-            >Lorem ipsum dolor sit amet consectetur adipisicing elit.</a
-          ><br /><br />
-          <a href="#" class="bi-chevron-right"
-            >Lorem ipsum dolor sit amet consectetur adipisicing elit.</a
-          ><br /><br />
-          <a href="#" class="bi-chevron-right"
-            >Lorem ipsum dolor sit amet consectetur adipisicing elit.</a
-          ><br /><br />
-        </div>
+            <div class="col-lg-5">
+              <div class="Blogs">
+                <label>Our Blogs</label><br />
+                <a href="#" class="bi-chevron-right">Lorem ipsum dolor, sit amet consectetur adipisicing
+                  elit.</a><br /><br />
+                <a href="#" class="bi-chevron-right">Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit.</a><br /><br />
+                <a href="#" class="bi-chevron-right">Lorem ipsum dolor sit, amet consectetur adipisicing
+                  elit.</a><br /><br />
+                <a href="#" class="bi-chevron-right">Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit.</a><br /><br />
+                <a href="#" class="bi-chevron-right">Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit.</a><br /><br />
+                <a href="#" class="bi-chevron-right">Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit.</a><br /><br />
+              </div>
+            </div>
 
-        <div class="footer-form">
-          <label>Leave Us a Message</label><br />
+            <div class="col-lg-4">
+              <div class="footer-form">
+                <label>Leave Us a Message</label><br />
 
-          <form>
-            <input
-              type="text"
-              name="name"
-              class="form-control"
-              placeholder="Your Name*"
-              required=""
-            />
-            <br />
-            <input
-              type="email"
-              class="form-control"
-              name="email"
-              placeholder="Your Email*"
-              required=""
-            />
-            <br />
-            <textarea
-              type="message"
-              class="form-control"
-              name="message"
-              rows="3"
-              placeholder="Message*"
-              required=""
-            ></textarea>
-            <br />
-            <button class="btn-all m-0 mb-3" type="submit">Send Message</button>
-          </form>
-        </div>
-      </div>
-    </section> 
-         <footer>
-            <p>Copyrights 2022 ©All Rights Reserved - Skill Select Australia</p>
-         </footer>
+                <form>
+                  <input type="text" name="name" class="form-control" placeholder="Your Name*" required="" />
+                  <br />
+                  <input type="email" class="form-control" name="email" placeholder="Your Email*" required="" />
+                  <br />
+                  <textarea type="message" class="form-control" name="message" rows="3" placeholder="Message*"
+                    required=""></textarea>
+                  <br />
+                  <button class="btn-all m-0 mb-3" type="submit">Send Message</button>
+                </form>
+              </div>
+            </div>
 
-         <div class="whatsapp-chatra">
-            <a
-                href="https://api.whatsapp.com/send?phone=61480097490&amp;text=Hello,%20Can%20you%20please%20provide%20me%20more%20information?"
-                class="back-to-whatsapp d-flex align-items-center justify-content-center"
-                id="whatsapp"
-                target="_blank"
-            ><i class="bi-whatsapp"></i
-            ></a>
+          </div>
         </div>
-
-        <div class="go-to-top">
-        <button
-            onclick="topFunction()"
-            id="goToTop"
-            title="Go to top"
-        >
-        <i
-          class="bi-arrow-up d-flex align-items-center justify-content-center"
-        ></i>
-        </button>
-        </div>
-        `;
+      </section>
+      <footer>
+        <p>Copyrights 2022 ©All Rights Reserved - Skill Select Australia</p>
+      </footer>
+    `;
   }
 }
 customElements.define("my-footer", MyFooter);

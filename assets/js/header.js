c408779f25c11732da22c9b3b539dbbbc22fb668
Kshitiz Rimal <kshitizrimal123@gmail.com>
2022-10-21 13:08:12 +0545

    hero sections

--- assets/js/header.js
+++ assets/js/header.js
@@ -4,7 +4,7 @@ class MyHeader extends HTMLElement {
 
   <nav class="navbar navbar-expand-lg" id="navbar">
     <div class="container">
-      <a class="navbar-brand" href="#">LOGO</a>
+      <a class="navbar-brand" href="./index.html">LOGO</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>

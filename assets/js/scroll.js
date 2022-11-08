a1a3a75045b1156ca05e46c9f6284e1d06ac1691
Kshitiz Rimal <kshitizrimal123@gmail.com>
2022-11-08 07:31:48 +0545

    overflow managed

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -11,6 +11,7 @@ function changeColor() {
     changeColorto();
   } else {
     for (let i = 0; i <= x.length; i++) {
+      // x[i].style.color = "#fff";
       x[i].style.color = "#fff";
     }
   }

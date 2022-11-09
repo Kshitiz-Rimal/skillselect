f096e5226490f106a81a3533faa24ae33d6bda9b
Kshitiz Rimal <kshitizrimal123@gmail.com>
2022-11-09 13:11:31 +0545

    managed whatsapp

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,28 +1,21 @@
 let x = document.querySelectorAll(".items");
 
 window.onscroll = function () {
-  changeColor();
-  // scrollFunction();
-};
-function changeColor() {
   document.getElementById("navbar").style.backgroundColor = "#006E9D";
   document.getElementById("colorswitch").src = "./img/logo_whiteBG.png";
+
   if (window.scrollY == 0) {
-    changeColorto();
+    document.getElementById("navbar").style.backgroundColor = "";
+    document.getElementById("colorswitch").src = "./img/Logo.png";
+    for (let i = 0; i < x.length; i++) {
+      x[i].style.color = "#000";
+    }
   } else {
-    for (let i = 0; i <= x.length; i++) {
-      // x[i].style.color = "#fff";
+    for (let i = 0; i < x.length; i++) {
       x[i].style.color = "#fff";
     }
   }
-}
-function changeColorto() {
-  document.getElementById("navbar").style.backgroundColor = "";
-  document.getElementById("colorswitch").src = "./img/Logo.png";
-  for (let i = 0; i <= x.length; i++) {
-    x[i].style.color = "#000";
-  }
-}
+};
 
 // whatsapp chatra
 const myTimeout = setTimeout(show, 20000);
